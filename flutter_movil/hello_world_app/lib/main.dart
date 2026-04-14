a3a64ad074a2d954353d042e8b7da3bc3ed93b31
AyrielNoriega <ingenierosenior.ecommerce@interrapidisimo.com>
2026-04-14 09:51:02 -0500

    Seccion 4: Flutter - Primeros pasos  - completada

---
+++ flutter_movil/hello_world_app/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_functions_screen.dart';
+// import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // primarySwatch: Colors.blue,
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue
+      ),
+      home: const CounterFunctionsScreen(),
+    );
+  }
+
+}
---
+++ flutter_movil/hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Counter Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+              Text('$clickCounter', style: const TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
+              Text('Click${clickCounter == 1 ? '' : 's'}', style: const TextStyle(fontSize: 25)),
+            ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                clickCounter++;
+              });
+            },
+            child: const Icon(Icons.plus_one),
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                clickCounter--;
+              });
+            },
+            child: const Icon(Icons.exposure_minus_1),
+          ),
+        ],
+      ),
+    );
+  }
+}
